5568af219d9313c9144c693a371015e6967f238d
ADAN GIOVANNI RAMIREZ ERIVES <a1124250019@utch.edu.mx>
2024-10-14 22:24:24 -0600

    La barra lateral ahora tiene transparencia y blur + los controles de zoom se movieron arriba a la derecha para que no se ocultara por la barra lateral

--- scripts/map.js
+++ scripts/map.js
@@ -1,8 +1,12 @@
-var map = L.map('map').setView([28.641874, -106.147054], 15);
+var map = L.map('map', {zoomControl: false}).setView([28.641874, -106.147054], 15);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.control.zoom({
+    position: 'topright' 
 }).addTo(map);
 
 L.marker([28.641874, -106.147054]).addTo(map);
