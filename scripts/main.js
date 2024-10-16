dac44d97301af2a34a17b3cf2539afa8bcd1f587
ADAN GIOVANNI RAMIREZ ERIVES <a1124250019@utch.edu.mx>
2024-10-15 22:06:04 -0600

    Agregué un menú desplegable al ícono de user, la estética de las barras cambió, tratemos de mejorarlo

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,19 @@
-const toggleBtn = document.getElementById('toggle-btn');
+const toggleBtn = document.getElementById('toggle-btn'); 
 const sidebar = document.getElementById('sidebar');
+const userButton = document.getElementById("usuario");
+const userMenu = document.getElementById("menu");
 
 toggleBtn.addEventListener('click', function() {
     sidebar.classList.toggle('hidden'); // Alterna la visibilidad de la barra lateral
+});
+
+userButton.addEventListener('click', function(event) {
+    event.stopPropagation(); // Prevenir que el evento se propague
+    userMenu.style.display = (userMenu.style.display === 'flex') ? 'none' : 'block'; // Alternar menú
+});
+
+window.addEventListener('click', function(event) {
+    if (!userButton.contains(event.target)) {
+        userMenu.style.display = 'none'; 
+    }
 });
