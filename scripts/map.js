3dc3bace6731bb37e0a790c24dc69ce841c28aa1
ADAN GIOVANNI RAMIREZ ERIVES <a1124250019@utch.edu.mx>
2024-10-16 22:14:23 -0600

    ESTÁ VIVOOOOOOOOOOOOOOO **gritos de emoción** Se agregaron las rutas y el motor routing machine de Leaflet para el trazado de rutas

--- scripts/map.js
+++ scripts/map.js
@@ -9,6 +9,21 @@ L.control.zoom({
     position: 'topright' 
 }).addTo(map);
 
-L.marker([28.641874, -106.147054]).addTo(map);
+// Configuración del control de enrutamiento
+var routingControl = L.Routing.control({
+    waypoints: [
+        L.latLng(28.606896,-106.097504), 
+        L.latLng(28.602196,-106.101084), 
+        L.latLng(28.6015419,-106.1026966),
+        L.latLng(28.647509, -106.133202), // El primer punto es el inicio, los demás son paradas
+        L.latLng(28.642570, -106.146970) // Destino final, UTCh
+    ],
+    draggableWaypoints: false,
+    routeWhileDragging: false,
+    lineOptions: {
+        styles: [{ color: 'blue', opacity: 0.7, weight: 5 }]
+    },
+    show: false
+}).addTo(map);
 
-L.popup().setLatLng([28.642874, -106.147054]).setContent('UTCh').openOn(map);
+L.marker([28.642570, -106.146970]).addTo(map).bindPopup("UTCh").openPopup()
