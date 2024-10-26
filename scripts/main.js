12086d5c4193640683b7ad139ac0ae398f2487f1
diegodealer <diego.rf726@gmail.com>
2024-10-26 13:03:57 -0600

    metodo de traductor mas eficiente

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@ const sidebar = document.getElementById('sidebar');
 const userButton = document.getElementById("usuario");
 const userMenu = document.getElementById("menu");
 const languageSwitcher = document.getElementById('languageSwitcher');
-const translations = {};
+let translations = {};
 
 
 toggleBtn.addEventListener('click', function() {
@@ -45,145 +45,86 @@ function loadLanguage(language) {
     fetch('/scripts/content.json')  
         .then(response => response.json())
         .then(data => {
-            const translations = data; // Suponiendo que content.json tiene traducciones solo en inglés
+            translations = data; // Guardar las traducciones en el ámbito global
             console.log('Traducciones cargadas:', translations);  
 
             // Actualizar contenido basado en el idioma seleccionado
-            updateContent(language, translations);
+            updateContent(language);
         })
         .catch(error => console.error('Error al cargar las traducciones:', error));
 }
 
-// Función para actualizar el contenido de la página
-function updateContent(language, translations) {
-    if (language === 'en') {
-        const tituloElement = document.getElementById('tituloprincipal');
-        if (tituloElement) tituloElement.textContent = translations[language].tituloprincipal; // asi es como se debe hacer para que aparezca el contenido
+let elementsMap = {
+    "tituloprincipal": ["#mainTitle"],
+    "titulo2": ["#subtitle"],
+    "profile_link": ["#profileLink"],
+    "selectoridioma": ["#languageSelector"],
+    "link_configuracion": ["#settingsLink"],
+    "link_cerrar-sesion": ["#logoutLink"],
+    "rutas_camiones": ["#busRoutesTitle"],
+    
+    "ruta_norte": ["#northRouteTitle"],
+    "ruta_norte-descripcion": ["#northRouteDescription"],
+    "rutan_parada1": ["#northStop1"],
+    "rutan_parada2": ["#northStop2"],
+    "rutan_parada3": ["#northStop3"],
+    "rutan_parada4": ["#northStop4"],
+    "rutan_salida": ["#northDeparture"],
+    "rutan_regreso": ["#northReturn"],
+    "rutan_costo": ["#northCost"],
+    
+    "ruta_noroeste": ["#northwestRouteTitle"],
+    "ruta_noroeste_desc": ["#northwestRouteDescription"],
+    "rutanoe_parada1": ["#northwestStop1"],
+    "rutanoe_parada2": ["#northwestStop2"],
+    "rutanoe_parada3": ["#northwestStop3"],
+    "rutanoe_parada4": ["#northwestStop4"],
+    "rutanoe_parada5": ["#northwestStop5"],
+    "rutanoe_parada6": ["#northwestStop6"],
+    "rutanoe_parada7": ["#northwestStop7"],
+    "rutanoe_parada8": ["#northwestStop8"],
+    "rutanoe_parada9": ["#northwestStop9"],
+    "rutanoe_parada10": ["#northwestStop10"],
+    "rutanoe_parada11": ["#northwestStop11"],
+    "rutanoe_parada12": ["#northwestStop12"],
+    "rutanoe_salida": ["#northwestDeparture"],
+    "rutanoe_regreso": ["#northwestReturn"],
+    "rutanoe_precio": ["#northwestCost"],
+    
+    "ruta_sur": ["#southRouteTitle"],
+    "sur_info1": ["#southInfo1"],
+    "sur_info2": ["#southInfo2"],
+    "rutasur_desc": ["#southRouteDescription"],
+    "rutasur_desc2": ["#southRouteDescription2"],
+    "rutasur_parada1": ["#southStop1"],
+    "rutasur_parada2": ["#southStop2"],
+    "rutasur_parada3": ["#southStop3"],
+    "rutasur_parada4": ["#southStop4"],
+    "rutasur_costo": ["#southCost"],
 
-        const titulo2Element = document.getElementById('titulo2');
-        if (titulo2Element) titulo2Element.textContent = translations[language].titulo2;
+    "rutasur_regreso": ["#southReturnTitle"],
+    "rutasur_reg-espc": ["#southReturnDescription"],
+    "rutasur_reg-parada1": ["#southReturnStop1"],
+    "rutasur_reg-parada2": ["#southReturnStop2"],
+    "rutasur_reg-parada3": ["#southReturnStop3"],
+    "rutasur_reg-parada4": ["#southReturnStop4"],
+    "rutasur_reg-parada5": ["#southReturnStop5"],
+    "rutasur_reg-parada6": ["#southReturnStop6"],
+    "rutasur_inicio": ["#southReturnDeparture"],
+    "rutasur_costo_reg": ["#southReturnCost"]
+};
 
-        const profileElement = document.getElementById('profile_link');
-        if (profileElement) profileElement.textContent = translations[language].profile_link;
 
-        const configElement = document.getElementById('selectoridioma');
-        if (configElement) configElement.textContent = translations[language].selectoridioma;
-
-        const selectidiomaElement = document.getElementById('link_configuracion');
-        if (selectidiomaElement) selectidiomaElement.textContent = translations[language].link_configuracion;
-
-        const logoutsesionElement = document.getElementById('link_cerrar_sesion');
-        if (logoutsesionElement) logoutsesionElement.textContent = translations[language].link_cerrar_sesion;
-
-        const BusroutesElement = document.getElementById('rutas_camiones');
-        if (BusroutesElement) BusroutesElement.textContent = translations[language].rutas_camiones;
-
-        const rutaNorteElement = document.getElementById('ruta_norte');
-        if (rutaNorteElement) rutaNorteElement.textContent = translations[language].ruta_norte;
-
-        const rutaNortdesElement = document.getElementById('ruta_norte_descripcion');
-        if (rutaNortdesElement) rutaNortdesElement.textContent = translations[language].ruta_norte_descripcion;
-
-        const rutaNp1Element = document.getElementById('rutan_parada1');
-        if (rutaNp1Element) rutaNp1Element .textContent = translations[language].rutan_parada1;
-
-        const rutaNp2Element = document.getElementById('rutan_parada2');
-        if (rutaNp2Element) rutaNp2Element.textContent = translations[language].rutan_parada2;
-
-        const rutaNp3Element = document.getElementById('rutan-parada3');
-        if (rutaNp3Element) rutaNp3Element.textContent = translations[language].rutan_parada3;
-
-        const rutaNp4Element = document.getElementById('rutan_parada4');
-        if (rutaNp4Element) rutaNp4Element.textContent = translations[language].rutan_parada4;
-
-        const rutaNSElement = document.getElementById('rutan_salida');
-        if (rutaNSElement) rutaNSElement.textContent = translations[language].rutan_salida;
-
-        const rutaNrgElement = document.getElementById('rutan_regreso');
-        if (rutaNrgElement) rutaNrgElement.textContent = translations[language].rutan_regreso;
-
-        const rutaNcostElement = document.getElementById('rutan_costo');
-        if (rutaNcostElement) rutaNcostElement.textContent = translations[language].rutan_costo;
-
-        const rutaNoeElement = document.getElementById('ruta_noroeste');
-        if (rutaNoeElement ) rutaNoeElement.textContent = translations[language].ruta_noroeste;
-
-        const rutaNoedescElement = document.getElementById('ruta_noroeste_desc');
-        if (rutaNoedescElement) rutaNoedescElement.textContent = translations[language].ruta_noroeste_desc;
-
-        const rutaNoep1Element = document.getElementById('rutanoe_parada1');
-        if (rutaNoep1Element) rutaNoep1Element.textContent = translations[language].rutanoe_parada1;
-
-        const rutaNoep2Element = document.getElementById('rutanoe_parada2');
-        if (rutaNoep2Element) rutaNoep2Element.textContent = translations[language].rutanoe_parada2;
-
-        const rutaNoep3Element = document.getElementById('rutanoe_parada3');
-        if (rutaNoep3Element) rutaNoep3Element.textContent = translations[language].rutanoe_parada3;
-
-        const rutaNoep4Element = document.getElementById('rutanoe_parada4');
-        if (rutaNoep4Element) rutaNoep4Element.textContent = translations[language].rutanoe_parada4;
-
-        const rutaNoep5Element = document.getElementById('rutanoe_parada1');
-        if (rutaNoep5Element) rutaNoep5Element.textContent = translations[language].rutanoe_parada5;
-
-        const rutaNoep6Element = document.getElementById('rutanoe_parada1');
-        if (rutaNoep6Element) rutaNoep6Element.textContent = translations[language].rutanoe_parada6;
-
-        const rutaNoep7Element = document.getElementById('rutanoe_parada7');
-        if (rutaNoep7Element) rutaNoep7Element.textContent = translations[language].rutanoe_parada7;
-
-        const rutaNoep8Element = document.getElementById('rutanoe_parada8');
-        if (rutaNoep8Element) rutaNoep8Element.textContent = translations[language].rutanoe_parada8;
-
-        const rutaNoep9Element = document.getElementById('rutanoe_parada9');
-        if (rutaNoep9Element) rutaNoep9Element.textContent = translations[language].rutanoe_parada9;
-
-        const rutaNoep10Element = document.getElementById('rutanoe_parada10');
-        if (rutaNoep10Element) rutaNoep10Element.textContent = translations[language].rutanoe_parada10;
-
-        const rutaNoep11Element = document.getElementById('rutanoe_parada11');
-        if (rutaNoep11Element) rutaNoep11Element.textContent = translations[language].rutanoe_parada11;
-
-        const rutaNoep12Element = document.getElementById('rutanoe_parada12');
-        if (rutaNoep12Element) rutaNoep12Element.textContent = translations[language].rutanoe_parada12;
-
-        const rutaNoeSAElement = document.getElementById('rutanoe_salida');
-        if (rutaNoeSAElement) rutaNoeSAElement.textContent = translations[language].rutanoe_salida;
-
-        const rutaNoeRegElement = document.getElementById('rutanoe_regreso');
-        if (rutaNoeRegElement) rutaNoeRegElement.textContent = translations[language].rutanoe_regreso;
-
-        const rutaNoecostElement = document.getElementById('rutanoe_precio');
-        if (rutaNoecostElement) rutaNoecostElement.textContent = translations[language].rutanoe_precio;
-
-        const rutaSurElement = document.getElementById('ruta_sur');
-        if (rutaSurElement) rutaSurElement.textContent = translations[language].ruta_sur;
-
-        /*
-      "ruta_sur":"Ruta Sur rumbo a la UTCH",
-      "sur_info1":"El primer camión de esta ruta comeinza en Alsuper Fuentes Mares",
-      "sur_info2":"El segundo camion de esta ruta comienza en frente de Terminal Sur J.P.II",
-      "rutasur_desc":"Ambas rutas comienzan su recorido a las 6:15Am, llegando a la UTCH a las 6:55Am",
-      "rutasur_desc2":"Las paradas de estas rutas son:",
-      "rutasur_parada1":"Av. Pacheco.",
-      "rutasur_parada2":"C. Juarez.",
-      "rutasur_parada3":"C. Neri Santos.",
-      "rutasur_parada4":"Av. Independencia.",
-      "rutasur_costo":"Ambas paradas tiene un costo de $15.00 pesos mx",
-
-  
-      "rutasur_regreso":"Ruta Sur de regreso de la UTCH",
-      "rutasur_reg-espc":"Esta ruta de regreso de la UTCH comienza su trayecto en  la UTCH y sus paradas son:",
-      "rutasur_reg-parada1":"Periférico frente a Fashion mall.",
-      "rutasur_reg-parada2":"Glorieta tricentenario.",
-      "rutasur_reg-parada3":"Vialidad CH-P y Fuentes Mares.",
-      "rutasur_reg-parada4":"Fuentes Mares y Nva. España.",
-      "rutasur_reg-parada5":"Terminar Sur.",
-      "rutasur_reg-parada6":"Pacheco y Pedro Meoqui.",
-      "rutasur_inicio":"Esta ruta comienza su trayecto a las 2:05Pm",
-      "rutasur_costo_reg":"La ruta Sur tiene un costo de $20.00 pesos mx"*/
-        
-        // Agrega más elementos aquí según sea necesario
-    } 
+function updateContent(language) {
+    for (const [translationKey, elementIds] of Object.entries(elementsMap)) {
+        elementIds.forEach(id => {
+            const element = document.querySelector(id);
+            if (element && translations[language] && translations[language][translationKey]) {
+                element.textContent = translations[language][translationKey];
+            } else {
+                console.warn(`No se encontró el elemento o la traducción para ${translationKey}`);
+            }
+        });
+    }
 }
 });
